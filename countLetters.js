86243a696a6461061d66963930757a318565f353
stephkri <stephane.krims@hotmail.ca>
2021-07-23 18:45:15 +0000

    countLetters and minor mods to countOnly

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function isLetter(c) {
+  return c.toLowerCase() != c.toUpperCase();
+}
+
+const countLetters = function(string) {
+  const results = {};
+  for (const letter of string) {
+    if (isLetter(letter)) {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
--- countOnly.js
+++ countOnly.js
@@ -12,10 +12,10 @@ if there is a key in itemsToCoun
 */
 
 const countOnly = function(allItems, itemsToCount) {
-  const results = {}
+  const results = {};
   for (const item of allItems) { // item is an element in the array allItems
     console.log(item);
-    if(itemsToCount[item]) { // if there is a key in itemsToCount with the same name as the selected element in allItems
+    if (itemsToCount[item]) { // if there is a key in itemsToCount with the same name as the selected element in allItems
       if (results[item]) { // if there has already been (in a previous iteration of the loop) a key-value pair added to the results object
         results[item] += 1; // increase the value of the key in the results object by 1
       } else {
