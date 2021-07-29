f4777b8af614de174aa31d5657d3a3a372360800
stephkri <stephane.krims@hotmail.ca>
2021-07-29 00:45:44 +0000

    map function

---
+++ map.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+
+const results2 = map(words, word => word.length);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+
+const results3 = map(words, word => word.toUpperCase());
+assertArraysEqual(results3, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
+
+const results4 = map(words, word => word.concat("a"));
+assertArraysEqual(results4, ["grounda", "controla", "toa", "majora", "toma"]);
