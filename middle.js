dde71b39a09865a19af12305d6a69f14b15415fa
stephkri <stephane.krims@hotmail.ca>
2021-08-09 21:35:14 +0000

    Added modular refactor to middle

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   let newArray = [];
   if (array.length >= 3) {
@@ -33,14 +13,7 @@ const middle = function(array) {
   return newArray;
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
 
 /*
 Mid index for odd number of elements:
