de2d14883fe4f4925e752dcff44c27ff0db06a6d
stephkri <stephane.krims@hotmail.ca>
2021-08-09 22:07:16 +0000

    Added npm, mocha and chai and refactored some tests accordingly

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,12 @@
+//const assert = require("chai").assert;
 const assertArraysEqual = require("../assertArraysEqual");
+/*
+describe("#assertArraysEqual", () => {
+  it("should pass the assertion for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual
+  })
+})
+*/
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,14 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
 
-assertEqual(tail([]).length, 0);
-assertEqual(tail([12]).length, 0);
+describe("#tail", () => {
+  it("should return 0 for tail([]).length", () => {
+    assert.equal(tail([]).length, 0);
+  });
+  it("should return 0 for tail([12]).length", () => {
+    assert.equal(tail([12]).length, 0);
+  });
+  it("should return [2, 3, 4] for tail([1, 2, 3, 4])", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+});
